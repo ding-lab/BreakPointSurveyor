dd2fec7000d6e7aa9a8dca3ec265ac7a92e06f3b
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2016-03-22 16:45:06 -0500

    Ad hoc changes for debugging.

--- M_Reference/2_get_merged_exon_bed.sh
+++ M_Reference/2_get_merged_exon_bed.sh
@@ -7,25 +7,38 @@ mkdir -p $TMP
 
 BIN="$BPS_CORE/src/annotation/GTFFilter.py"
 
-#GTF="$OUTD/Homo_sapiens.GRCh37.75.gtf"
-GTF="$OUTD/Homo_sapiens.GRCh38.84.gtf"
-GTF="$OUTD/Homo.100000.gtf"
+function define_75 {
+    #GTF="$OUTD/Homo_sapiens.GRCh37.75.gtf"
+    GTF="$OUTD/RAD51B.37.75.gtf"
+    OUT="$OUTD/exons.ens75.bed"
+    ARGS="-e 75"
+}
 
-#OUT="$OUTD/exons.ens75.bed"
-OUT="$OUTD/exons.ens84.bed"
-  
+function define_84 {
+    # GTF="$OUTD/Homo_sapiens.GRCh38.84.gtf"
+    GTF="$OUTD/RAD51B.38.84.gtf"
+    OUT="$OUTD/exons.ens84.bed"
+    ARGS="-e 84"
+}
+
 TMP1="$TMP/all.exons.bed"        # temporary BED file of unsorted and unmerged exons
 TMP2="$TMP/merged.exons.bed"     # temporary BED file of merged but unsorted exons
 rm -f $TMP2; touch $TMP2
 
-python $BIN -s -b exon  < $GTF 
-exit
+#define_75
+define_84
+
+#python $BIN $ARGS -s -b exon  < $GTF 
+python $BIN $ARGS -s -b exon  < $GTF | bedtools sort -i stdin > $TMP1
+
 #python $BIN -s -b exon  < $GTF | bedtools sort -i stdin > $TMP1
 echo Written to $TMP1
 
 # now go through genes one by one and and merge all their beds together
 for GENE in `cut -f 4 $TMP1 | sort -u `; do
-fgrep -w $GENE $TMP1 | mergeBed -nms -i stdin | sed 's/;.*//' >> $TMP2
+#fgrep -w $GENE $TMP1 | mergeBed -nms -i stdin | sed 's/;.*//' >> $TMP2
+#fgrep -w $GENE $TMP1 | mergeBed -c 4 -o collapse  -i stdin | sed 's/;.*//' >> $TMP2
+fgrep -w $GENE $TMP1 | mergeBed -c 4 -o collapse  -i stdin | sed 's/,.*//' >> $TMP2
 done
 echo Written to $TMP2
 
