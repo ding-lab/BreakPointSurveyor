d6b4c6e1301640a7ba9213aae6501e94aec2507f
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2016-07-19 17:43:06 -0500

    Creating stub for Pindel processing

--- C_PindelRP/2_process_PindelRP.sh
+++ C_PindelRP/2_process_PindelRP.sh
@@ -4,23 +4,23 @@
 
 source ./PindelRP.config
 
-DATA_LIST="$BPS_DATA/A_Project/dat/1000SV.samples.dat"
+DATA_LIST="$OUTD/TCGA_Virus.Pindel_RP.dat"
 BIN="$BPS_CORE/src/analysis/Pindel_RP.Reader.R"
 echo Data list: $DATA_LIST
 echo \$BIN: $BIN
 
 while read l; do
-# barcode	bam_path	CTX_path	Pindel_path
 [[ $l = \#* ]] && continue
 [[ $l = barcode* ]] && continue
 
 BAR=`echo $l | awk '{print $1}'`
-PIN_FN=`echo $l | awk '{print $4}'`   # read column 3
+PIN_FN=`echo $l | awk '{print $2}'`   
 
 ARGS="-S" 
 OUT="$OUTD/${BAR}.PindelRP.BPR.dat"
 
 # keep only first 12 columns of Pindel_RP file
+#cut -f 1-12 $PIN_FN | Rscript $BIN $ARGS stdin $OUT
 cut -f 1-12 $PIN_FN | Rscript $BIN $ARGS stdin $OUT
 
 echo Written to $OUT
