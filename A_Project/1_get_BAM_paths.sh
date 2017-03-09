f72dee936efe70a3ac09796f6448e0c659597645
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-03-09 15:33:23 -0600

    Introducting data file name changes to 1000SV branch

--- A_Project/1_get_BAM_paths.sh
+++ A_Project/1_get_BAM_paths.sh
@@ -1,12 +1,14 @@
 # List of paths to aligned BAM files
 
+# for 1000SV, reference obtained from alignment file
+
 source ./Project.config
 
 OUT="$OUTD/WGS.samples.dat"
 
 echo -e "barcode\tdisease\tBAM_path\tref_path" > $OUT
 cat <<EOF | sort | tr ' ' '\t' >> $OUT
-TCGA-BA-4077-01B-01D-2268-08 HNSC /home/archive/mwyczalk/BreakPointSurveyor.TCGA_Data/248c12a95d464254bc62d627d3db8152.bam /home/archive/mwyczalk/BreakPointSurveyor.TCGA_Data/all_sequences.fa
+NA19240 NA /diskmnt/Datasets/1000G_SV/ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/hgsv_sv_discovery/data/YRI/NA19240/high_cov_alignment/NA19240.alt_bwamem_GRCh38DH.20150715.YRI.high_coverage.cram ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
 EOF
 
 echo Written to $OUT
