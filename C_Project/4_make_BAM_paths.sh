f0fd1d363735b52f3b3654443a3e821e710d1099
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-03-15 16:29:23 -0500

    Full analysis of synthetic dataset
    * All intermediate files retained

--- C_Project/4_make_BAM_paths.sh
+++ C_Project/4_make_BAM_paths.sh
@@ -5,7 +5,7 @@ OUT="$OUTD/WGS.samples.dat"
 
 echo -e "barcode\tdisease\tBAM_path\tref_path" > $OUT
 cat <<EOF | sort | tr ' ' '\t' >> $OUT
-synthetic.9-22 NA $OUTD/synthetic.BWA.bam $BPS_DATA/A_Reference/dat/reference.chr9_chr22.fa
+synthetic.9-22 NA $OUTD/synthetic.BWA.bam reference.chr9_chr22.fa
 EOF
 
 echo Written to $OUT
