941bf5cd830ec715a5e926ce5c03cb843795734a
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-03-20 10:43:20 -0500

    Adding discordant reads from AU, AQ events

--- C_Project/3_get_BAM_paths.sh
+++ C_Project/3_get_BAM_paths.sh
@@ -8,8 +8,8 @@ OUT="$OUTD/WGS.samples.dat"
 
 echo -e "barcode\tdisease\tBAM_path\tref_path" > $OUT
 cat <<EOF | sort | tr ' ' '\t' >> $OUT
-NA19240 NA $OUTD/NA19240.AQ.bam GRCh38_full_analysis_set_plus_decoy_hla.fa
-NA19240 NA $OUTD/NA19240.AU.bam GRCh38_full_analysis_set_plus_decoy_hla.fa
+NA19240.AQ NA $OUTD/NA19240.AQ.bam GRCh38_full_analysis_set_plus_decoy_hla.fa
+NA19240.AU NA $OUTD/NA19240.AU.bam GRCh38_full_analysis_set_plus_decoy_hla.fa
 EOF
 
 echo Written to $OUT
