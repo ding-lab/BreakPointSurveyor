ba4cd944d3af902ab2f904ec6c0af3d5e73ac2f4
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-04-03 13:05:29 -0500

    Bugfix to manual creation of PlotList

--- C_Project/4_make_BAM_paths.sh
+++ C_Project/4_make_BAM_paths.sh
@@ -8,8 +8,8 @@ source ./BPS_Stage.config
 
 U_OUTD="$BPS_DATA/C_Project/dat.untracked"
 
-echo -e "barcode\tdisease\tBAM_path\tref_path" > $SAMPLE_LIST
-cat <<EOF | sort | tr ' ' '\t' >> $SAMPLE_LIST
+cat <<EOF | sort | tr ' ' '\t' > $SAMPLE_LIST
+barcode disease BAM_path ref_path
 NA19240.AQ NA $U_OUTD/NA19240.AQ.bam $BPS_DATA/A_Reference/dat.untracked/GRCh38_full_analysis_set_plus_decoy_hla.fa
 NA19240.AU NA $U_OUTD/NA19240.AU.bam $BPS_DATA/A_Reference/dat.untracked/GRCh38_full_analysis_set_plus_decoy_hla.fa
 EOF
